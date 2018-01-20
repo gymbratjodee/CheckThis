7a6e1a779b4edd8758028b5e01d186f09048ce84
JP <gymbratjodee@yahoo.com>
2018-01-20 00:57:58 -0800

    HitDB

---
+++ Tests/ListsManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using Lists;
+
+namespace Tests
+{
+    [TestClass]
+    public class ListsManagerTests
+    {
+        private ListsManager _listsManager;
+
+        public ListsManagerTests()
+        {
+            _listsManager = new ListsManager();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var results = _listsManager.CountListsTest();
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results > 0);
+        }
+
+
+    }
+}
